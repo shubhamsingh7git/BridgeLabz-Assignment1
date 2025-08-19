9e8cd381d95af10044d9cc2d669b190ebd163196
shubhamsingh7git <iamshubhamsingh0405@gmail.com>
2025-08-19 09:32:57 +0530

    Initial Commit

---
+++ Day_2/Q5.java
@@ -0,0 +1,15 @@
+package Day_2;
+
+class Try{
+    static int num=1;
+}
+
+public class Q5 {
+    public static void main(String[] args) {
+        Try obj1=new Try();
+        Try obj2=new Try();
+        obj1.num=10;
+        System.out.println(obj2.num);
+    }
+    
+}
---
+++ Day_2/Q6.java
@@ -0,0 +1,18 @@
+package Day_2;
+
+class Demo{
+    int num=5;
+
+    Demo(int num){
+        System.out.println(this.num);
+        System.out.println(num);
+        
+    }
+}
+
+public class Q6 {
+    public static void main(String[] args) {
+        Demo obj1= new Demo(61);
+    }
+    
+}
